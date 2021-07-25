47f26cc8831798d51ec11cd27e51867ff366a195
Ed Yoakum <eyoakum620@gmail.com>
2021-07-24 20:58:29 -0400

    initial xcode project creation

---
+++ CWC_OneDay_Build_20210724App.swift
@@ -0,0 +1,17 @@
+//
+//  CWC_OneDay_Build_20210724App.swift
+//  CWC-OneDay_Build_20210724
+//
+//  Created by Ed Yoakum on 7/24/21.
+//
+
+import SwiftUI
+
+@main
+struct CWC_OneDay_Build_20210724App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CWC-OneDay_Build_20210724
+//
+//  Created by Ed Yoakum on 7/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
